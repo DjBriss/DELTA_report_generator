df20f1df7946c9867013b1d08f11f715d354e07d
DjBriss <jeremie.s.brisson@gmail.com>
2017-07-23 11:04:34 -0700

    Add files via upload

---
+++ WindowsApplication2/WindowsApplication2/Form1.Designer.vb
@@ -0,0 +1,94 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.DateTimePicker1 = New System.Windows.Forms.DateTimePicker()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.ContextMenuStrip1 = New System.Windows.Forms.ContextMenuStrip(Me.components)
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.SuspendLayout()
+        '
+        'DateTimePicker1
+        '
+        Me.DateTimePicker1.Location = New System.Drawing.Point(37, 43)
+        Me.DateTimePicker1.Name = "DateTimePicker1"
+        Me.DateTimePicker1.Size = New System.Drawing.Size(200, 20)
+        Me.DateTimePicker1.TabIndex = 0
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(85, 70)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "Button1"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(85, 13)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(39, 13)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Label1"
+        '
+        'ContextMenuStrip1
+        '
+        Me.ContextMenuStrip1.Name = "ContextMenuStrip1"
+        Me.ContextMenuStrip1.Size = New System.Drawing.Size(61, 4)
+        '
+        'ListBox1
+        '
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.Items.AddRange(New Object() {"C:\Documents and Settings\operator\My Documents\DELTA\reports\ ", "C:\Users\Jbrisson\Documents\Visual Studio 2015\Projects\WindowsApplication2\Windo" &
+                "wsApplication2\bin\Debug\JULY2017"})
+        Me.ListBox1.Location = New System.Drawing.Point(69, 99)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(120, 56)
+        Me.ListBox1.TabIndex = 3
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(284, 180)
+        Me.Controls.Add(Me.ListBox1)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.DateTimePicker1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents DateTimePicker1 As DateTimePicker
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents ContextMenuStrip1 As ContextMenuStrip
+    Friend WithEvents ListBox1 As ListBox
+End Class
---
+++ WindowsApplication2/WindowsApplication2/Form1.vb
@@ -0,0 +1,129 @@
+﻿Public Class Form1
+    'So basically the user chooses the Date, Then file location, Then presses the button To start the process, cause that how I figured it out
+    '    The process starts by making a New big array, Then fills it With the existing report, Then eventually overwrites the file And add a New column
+    '    based ReadOnly the numbers taken from a parsed csv file that I add To the big array
+    Dim file_location As String = ""
+    Dim day_num As Integer = 1
+
+    Dim month_num As String = ""
+
+    Dim year_num As String = ""
+
+    Private Sub ListBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ListBox1.SelectedIndexChanged
+        Console.WriteLine(ListBox1.SelectedIndex)
+
+        Dim file_location As String = ""
+
+        If ListBox1.SelectedIndex = 0 Then
+            file_location = "C:\Documents and Settings\operator\My Documents\DELTA\reports\ "
+        End If
+        If ListBox1.SelectedIndex = 1 Then
+
+            file_location = "C:\Users\Jbrisson\Documents\Visual Studio 2015\Projects\WindowsApplication2\WindowsApplication2\bin\Debug\"
+        End If
+
+        Console.WriteLine(file_location)
+    End Sub
+
+    Private Sub DateTimePicker1_ValueChanged(sender As Object, e As EventArgs) Handles DateTimePicker1.ValueChanged
+        'when the date is picked, the values are parced, and stored into the variables day_num, month_num, year_num for later use
+
+        Dim Datestring As String = DateTimePicker1.Value
+
+        Dim DATE_array As String() = Strings.Split(Datestring, "/")
+        Dim index As Integer
+        While index < 3
+            Console.WriteLine(DATE_array(index))
+            index += 1
+        End While
+
+        day_num = CInt(DATE_array(0))
+
+        month_num = MonthName(CInt(DATE_array(1)))
+
+        year_num = (Strings.Split(DATE_array(2), " ")(0))
+        '^ puts the year only as the variable, because the string also contains the time
+        Console.WriteLine(month_num)
+        Console.WriteLine(year_num)
+
+
+    End Sub
+
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        'starts the process
+
+        Dim old_report(100)() As String
+
+        read_file(file_location, old_report)
+        '^uses the function to fill the array with existing numbers from the report file in appropriate location
+
+        ' write_tofile(file_location, old_report)
+
+    End Sub
+
+    Private Function read_file(ByVal filelocation As String, ParamArray report As String()())
+        'to get the old report numbers
+
+        Console.WriteLine(filelocation & month_num & year_num & "\" & "report" & month_num & year_num & ".txt")
+
+        Dim filereader_1 As System.IO.StreamReader = My.Computer.FileSystem.OpenTextFileReader(filelocation & month_num & year_num & "\" & "report" & month_num & year_num & ".txt")
+        'starts to open the present version of the report, based on where it is stored
+
+        Dim i As Integer = 0
+        Dim j As Integer = 1
+        Do
+            Do
+
+                report(i)(j) = Strings.Split(filereader_1.ReadLine, "    ")(j)
+
+                j += 1
+                'inputs the numbers already present in the reports
+
+            Loop Until Strings.Split(filereader_1.ReadLine, "    ")(j) = ""
+
+            i += 1
+        Loop Until filereader_1.Peek = -1
+
+        'To test
+
+        Dim filereader_2 As System.IO.StreamReader = My.Computer.FileSystem.OpenTextFileReader(filelocation & month_num & year_num & "\" & "report" & month_num & year_num & ".txt")
+        Do
+            Do
+
+                Console.WriteLine(report(i)(j))
+                Console.WriteLine(TAB)
+
+                j += 1
+                'inputs the numbers already present in the reports
+
+            Loop Until report(i)(j) = ""
+
+            i += 1
+        Loop Until filereader_1.Peek = -1
+
+        Return report
+
+    End Function
+
+    'Private Function write_tofile(ByVal filelocation As String, ParamArray report As String()())
+
+    '    'to write the new report, with the added column
+    '    Dim filewriter As System.IO.StreamWriter = My.Computer.FileSystem.OpenTextFileWriter(filelocation)
+
+    '    Dim countercolumn As Integer = 0
+    '    Dim counterline As Integer = 0
+    '    Do
+    '        Do
+    '            filewriter.WriteLine(report(counterline)(countercolumn) + TAB())
+    '            countercolumn += 1
+    '            'iterate to write the first line of the report
+    '        Loop Until countercolumn = 33
+
+    '        'iterate to write all lines
+    '        counterline += 1
+
+    '    Loop Until counterline = 100
+
+    'End Function
+End Class
