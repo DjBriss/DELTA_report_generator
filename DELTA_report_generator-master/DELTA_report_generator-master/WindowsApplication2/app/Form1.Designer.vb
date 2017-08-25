d7e05be0315ba1431c741cfd35054ce39e8aab3d
DjBriss <jeremie.s.brisson@gmail.com>
2017-08-25 07:24:43 -0700

    Add files via upload

---
+++ DELTA_report_generator-master/DELTA_report_generator-master/WindowsApplication2/app/Form1.Designer.vb
@@ -0,0 +1,107 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.DateTimePicker1 = New System.Windows.Forms.DateTimePicker()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.ContextMenuStrip1 = New System.Windows.Forms.ContextMenuStrip(Me.components)
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.ListBox2 = New System.Windows.Forms.ListBox()
+        Me.SuspendLayout()
+        '
+        'DateTimePicker1
+        '
+        Me.DateTimePicker1.Location = New System.Drawing.Point(37, 43)
+        Me.DateTimePicker1.Name = "DateTimePicker1"
+        Me.DateTimePicker1.Size = New System.Drawing.Size(232, 20)
+        Me.DateTimePicker1.TabIndex = 0
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(37, 70)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(231, 23)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "Button1"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(132, 7)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(39, 13)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Label1"
+        '
+        'ContextMenuStrip1
+        '
+        Me.ContextMenuStrip1.ImageScalingSize = New System.Drawing.Size(20, 20)
+        Me.ContextMenuStrip1.Name = "ContextMenuStrip1"
+        Me.ContextMenuStrip1.Size = New System.Drawing.Size(61, 4)
+        '
+        'ListBox1
+        '
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.Items.AddRange(New Object() {"Lodge Mech Room", "C:\Users\Jbrisson\Documents\Visual Studio 2015\Projects\WindowsApplication2\Windo" &
+                "wsApplication2\bin\Debug\JULY2017", "HPC Mech Room"})
+        Me.ListBox1.Location = New System.Drawing.Point(37, 99)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(232, 56)
+        Me.ListBox1.TabIndex = 3
+        '
+        'ListBox2
+        '
+        Me.ListBox2.FormattingEnabled = True
+        Me.ListBox2.Items.AddRange(New Object() {"Excel 2003, (.xls)", "Excel 2013, (.xlsx)"})
+        Me.ListBox2.Location = New System.Drawing.Point(37, 162)
+        Me.ListBox2.Name = "ListBox2"
+        Me.ListBox2.Size = New System.Drawing.Size(231, 95)
+        Me.ListBox2.TabIndex = 4
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(310, 291)
+        Me.Controls.Add(Me.ListBox2)
+        Me.Controls.Add(Me.ListBox1)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.DateTimePicker1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents DateTimePicker1 As DateTimePicker
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents ContextMenuStrip1 As ContextMenuStrip
+    Friend WithEvents ListBox1 As ListBox
+    Friend WithEvents ListBox2 As ListBox
+End Class
---
+++ DELTA_report_generator-master/DELTA_report_generator-master/WindowsApplication2/app/Form1.vb
@@ -0,0 +1,192 @@
+﻿Imports System.IO
+Imports Microsoft.Office.Interop.Excel
+
+Public Class Form1
+    'So basically the user chooses the Date, Then file location, Then presses the button To start the process, cause that how I figured it out
+    '    The process starts by making a New big array, Then fills it With the existing report, Then eventually overwrites the file And add a New column
+    '    based ReadOnly the numbers taken from a parsed csv file that I add To the big array
+
+
+    ''initialize the necessary variables
+    Dim file_location As String = ""
+
+    Dim day_num As String = "1"
+
+    Dim month_num As String = ""
+
+    Dim year_num As String = ""
+
+    Dim day As Integer = 0
+
+    Dim extension As String = ".xlsx"
+
+    Dim excel As Application = New Application
+
+    'sets the extension
+    Public Sub ListBox2_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ListBox2.SelectedIndexChanged
+
+        If ListBox2.SelectedIndex = 0 Then
+            extension = ".xls"
+
+        ElseIf ListBox2.SelectedIndex = 1 Then
+            extension = ".xlsx"
+
+        End If
+
+        MsgBox(extension)
+    End Sub
+
+    'sets the location
+    Public Sub ListBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ListBox1.SelectedIndexChanged
+
+        If ListBox1.SelectedIndex = 0 Then
+            file_location = "C:\Documents and Settings\operator\My Documents\DELTA_reports\"
+        End If
+
+        If ListBox1.SelectedIndex = 1 Then
+            file_location = "C:\Users\Jbrisson\Documents\Visual Studio 2015\Projects\DELTA_report_generator-master\DELTA_report_generator-master\app\app\bin\Debug\"
+        End If
+
+        If ListBox1.SelectedIndex = 2 Then
+            file_location = "C:\Documents and Settings\Maintenance\My Documents\DELTA_reports\"
+        End If
+
+        MsgBox(file_location)
+    End Sub
+
+    'assigns values to the date variables
+    Public Sub DateTimePicker1_ValueChanged(sender As Object, e As EventArgs) Handles DateTimePicker1.ValueChanged
+        'when the date is picked, the values are parsed, and stored into the variables day_num, month_num, year_num for later use
+
+        Dim Datestring As String = DateTimePicker1.Value
+        Dim DATE_array As String() = Strings.Split(Datestring, "/")
+        Dim index As Integer
+        While index < 3
+            index += 1
+        End While
+
+        day_num = DATE_array(0)
+        month_num = MonthName(CInt(DATE_array(1)))
+        year_num = (Strings.Split(DATE_array(2), " ")(0))
+
+        '^ puts the year only as the variable, because the string also contains the time
+
+        day = CInt(day_num)
+
+    End Sub
+
+    'starts the process
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        excel_quit()
+
+        Dim monthly_report(100, 60) As Object
+        Dim daily_report(1000, 10) As Object
+        Dim numbersfilename As String = file_location & month_num & year_num & "\" & day_num & extension
+        Dim reportfilename As String = file_location & month_num & year_num & "\" & "monthly_report" & extension
+        Dim count As Integer = 0
+
+        If File.Exists(numbersfilename) = False Or File.Exists(reportfilename) = False Then
+            MsgBox("file not found")
+            Exit Sub
+        End If
+
+        daily_report = excel_into_array(daily_report, numbersfilename, 1)
+        count = number_of_sheets(reportfilename)
+        MsgBox(count)
+
+        For a As Integer = 1 To count
+
+            monthly_report = excel_into_array(monthly_report, reportfilename, a)
+            add_numbers(monthly_report, daily_report)
+            array_into_excel(monthly_report, reportfilename, a)
+
+        Next
+
+        excel_quit()
+
+        MsgBox("The numbers of report " & day_num & " have been added succesfully")
+
+    End Sub
+
+    Private Function excel_into_array(report(,) As Object, filename As String, i As Integer)
+
+        If File.Exists(filename) = False Then
+            MsgBox("file not found")
+            Exit Function
+        Else
+
+            Dim workbook1 As Workbook = excel.Workbooks.Open(filename)
+            Dim sheet As Worksheet = workbook1.Sheets(i)
+            Dim r As Range = sheet.UsedRange
+
+            report = r.Value(XlRangeValueDataType.xlRangeValueDefault)
+
+        End If
+        Return report
+
+    End Function
+
+    Private Function number_of_sheets(filename As String)
+
+        If File.Exists(filename) = False Then
+            MsgBox("file not found")
+            Exit Function
+        Else
+
+            Dim workbook0 As Workbook = excel.Workbooks.Open(filename)
+            Return workbook0.Sheets.Count
+
+        End If
+
+    End Function
+
+    Private Function add_numbers(monthly_report(,) As Object, daily_report(,) As Object)
+
+        If daily_report Is Nothing Then
+            MsgBox("No Numbers To add")
+            Exit Function
+        End If
+
+        Dim bound0 As Object = daily_report.GetUpperBound(0)
+        Dim bound1 As Object = monthly_report.GetUpperBound(0)
+        Dim location As Integer = day + 3
+
+        For index1 As Integer = 1 To bound1
+
+            For index2 As Integer = 1 To bound0
+
+                If System.String.Equals(monthly_report(index1, 3), daily_report(index2, 1)) = True And Not String.Equals(monthly_report(index1, 3), Nothing) Then
+
+                    monthly_report(index1, location) = daily_report(index2, 5)
+
+                End If
+
+            Next
+
+        Next
+
+    End Function
+
+    Private Function array_into_excel(report(,) As Object, filelocation As String, i As Integer)
+
+        If report Is Nothing Then
+            MsgBox("No Numbers To add")
+            Exit Function
+        End If
+
+        Dim workbook2 As Workbook = excel.Workbooks.Open(filelocation)
+        Dim sheet1 As Worksheet = workbook2.Sheets(i)
+
+        sheet1.UsedRange.Value = report
+
+        workbook2.Close(True,)
+
+    End Function
+
+    Sub excel_quit()
+        excel.Quit()
+        MsgBox("excel is dead")
+    End Sub
+
+End Class
